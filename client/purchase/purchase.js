391bd3ef71b7e89adf804e4bbdb44db76099521b
dylan <dylan.kojicheslin@gmail.com>
2016-09-09 02:01:26 -0600

    cruft

--- client/purchase/purchase.js
+++ client/purchase/purchase.js
@@ -1,12 +1,12 @@
 Template.purchase.events({
     'click #purchase-button': function(e) {
       e.preventDefault();
-      realKey = Meteor.settings.public.stripe.testPublishableKey
+      realKey = Meteor.settings.public.stripe.testPublishableKey;
 
       StripeCheckout.open({
         key: realKey,
         amount: 350, // tree fidy
-        name: 'test strip',
+        name: 'test stripe',
         description: 'I need about 3 fiddy',
         panelLabel: 'Pay Now',
         token: function(res) {
